bad77bd87b0ec9d4aa51945eea7c37a57db218c9
AarshShah9 <aarshshah99@gmail.com>
2025-12-22 10:31:39 -0600

    chore: refactored spotify code into subomodule

--- src/lib/spotify/index.ts
+++ src/lib/spotify/index.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { env } from "./env";
+import { env } from "../env";
 import { SpotifyCallbackResponseSchema, SpotifyFollowingResponseSchema, SpotifyUserSchema } from "./types";
 
 export const SPOTIFY_SCOPES = [
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -10,13 +10,6 @@ export const LinkCallbackSchema = z.object({
   state: z.coerce.string(),
 });
 
-export const SpotifyCallbackResponseSchema = z.object({
-  access_token: z.string(),
-  token_type: z.string(),
-  scope: z.string(),
-  expires_in: z.number(),
-  refresh_token: z.string(),
-});
 
 export const ConcertsQuerySchema = z
   .object({
@@ -43,62 +36,4 @@ export const ConcertsQuerySchema = z
         message: "end_date must be on or after start_date",
       });
     }
-  });
-
-export type ConcertsQuery = z.infer<typeof ConcertsQuerySchema>;
-
-export const SpotifyFollowingResponseSchema = z.object({
-  artists: z.object({
-    href: z.string(),
-    limit: z.number(),
-    next: z.string().nullable(),
-    cursors: z.object({
-      after: z.string().nullable(),
-    }),
-    total: z.number(),
-    items: z.array(z.object({
-      id: z.string(),
-      name: z.string(),
-      type: z.string(),
-      images: z.array(z.object({
-        url: z.string(),
-        height: z.number(),
-        width: z.number(),
-      })),
-      popularity: z.number(),
-      uri: z.string(),
-    })),
-  }),
 });
-
-export const SpotifyUserSchema = z.object({
-  country: z.string().length(2), 
-  display_name: z.string().nullable(),
-  email: z.string().email(), 
-  explicit_content: z.object({
-    filter_enabled: z.boolean(),
-    filter_locked: z.boolean(),
-  }),
-  external_urls: z.object({
-    spotify: z.string().url(),
-  }),
-  followers: z.object({
-    href: z.string().nullable(), 
-    total: z.number().int().nonnegative(),
-  }),
-  href: z.url(),
-  id: z.string(),
-  images: z.array(
-    z.object({
-      url: z.url(),
-      height: z.number().int().nullable(),
-      width: z.number().int().nullable(),
-    })
-  ),
-  product: z.enum(["premium", "free", "open"]),
-  type: z.literal("user"),
-  uri: z.string(),
-});
-
-// Type inference
-export type SpotifyUser = z.infer<typeof SpotifyUserSchema>;
